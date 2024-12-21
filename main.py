6ef7da01a325dfbed0b2301e40e128a76b4b35dd
Diegomendes0706 <diego.mendesgarcia@gmail.com>
2024-12-21 16:17:41 -0300

    notas e listagem

--- main.py
+++ main.py
@@ -1,22 +1,38 @@
-from os import listdir
-import os
+from glob import glob
 import scipy.io.wavfile as wavfile
 import numpy as np
-from numpy.fft import fft
+from matplotlib import pyplot as plt
+import matplotlib
+from scipy.fftpack import fft
+import pandas as pd
 
 if __name__ == '__main__':
-    # pegar arquivos de som
-    path_sounds = r'.\Violão'
-    sound_files = listdir(path_sounds)
-
-    n = 65536  # 16 bits
-
+    matplotlib.use('TkAgg')
+    # pegar arquivos
+    notes = pd.read_csv('notas.csv')
+    sound_files = glob(r'./Violão/*.wav')
     frequency = 16_000
-    time_interval = np.linspace(0, 10, n)
+    period = 1 / frequency
 
     sound_data = [
-        wavfile.read(os.path.join(path_sounds, s))[1]
+        wavfile.read(s)[1]
         for s in sound_files
     ]
+    audio_lengths = [
+        len(sound_data[d]) / frequency
+        for d in range(len(sound_data))
+    ]
+    time_intervals = [
+        np.arange(0, audio_lengths[a], period)
+        for a in range(len(audio_lengths))
+    ]
+    frequencies = [
+        (np.fft.fftfreq(len(sound_data[d]), period))
+        for d in range(len(sound_data))
+    ]
+    f_hats = [fft(d) for d in sound_data]
+    spectral_densities = [abs(fh) for fh in f_hats]
 
-    f_hats = [abs(fft(d)) for d in sound_data]
+    plt.figure()
+    plt.plot(frequencies[0], spectral_densities[0])
+    plt.show()
